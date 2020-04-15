245ca4a75aae32326be6788625fd15debc747ca7
Lukas Winkler <git@lw1.at>
2020-04-15 20:09:16 +0200

    fix colors

--- colors.py
+++ colors.py
@@ -1,10 +1,11 @@
 from data import colors_c2
-from models import Person
+from models import Person, Series
 
 p: Person
 for p in Person.select():
+    campaign2 = Series.select().where(Series.title == "Campaign 2")
     print(p)
-    if p.name in colors_c2.keys() and p.season == 2:
+    if p.name in colors_c2.keys() and p.series == campaign2:
         print(p.name)
         p.color = colors_c2[p.name]
         p.save()
