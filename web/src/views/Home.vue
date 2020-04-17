a69cc8dc570048301a8517493be12dc587457048
Lukas Winkler <git@lw1.at>
2020-04-17 18:00:31 +0200

    minor style changes

--- web/src/views/Home.vue
+++ web/src/views/Home.vue
@@ -1,112 +1,117 @@
 <template>
-    <div class="home">
-        <div class="title">
-            <h1>Critical Role Search</h1>
-            <span>Find your favourite Critical Role quote!</span>
-        </div>
-        <transition name="fade">
-            <div id="page-mask" v-if="showIntro || showYtOptIn"></div>
-        </transition>
-        <div v-if="showIntro" class="showIntro popup">
-            <div class="title"><h1>Critical Role Search</h1></div>
-            <div>
-
-                <p>
-                    This website is licensed under the
-                    <a href="https://www.gnu.org/licenses/gpl-3.0.en.html">GPL-3.0</a>.
-                    You can find the source <a href="#">here</a>.
-                </p>
-                <dl>
-                    <dt>Fonts</dt>
-                    <dd>
-                        <a href="https://github.com/jonathonf/solbera-dnd-fonts" target="_blank" rel="noopener">
-                            by Solbera and Ryrok
-                        </a> (CC BY-SA 4.0)
-                    </dd>
-                    <dt>Design</dt>
-                    <dd>
-                        Inspired by
-                        <a href="https://homebrewery.naturalcrit.com/" target="_blank" rel="noopener">
-                            The Homebrewery
-                        </a>
-                        (MIT License)
-                    </dd>
-                </dl>
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab amet aspernatur at deleniti dignissimos
-                    est
-                    facilis hic in mollitia nam odit, porro provident quis quisquam rem sit tempora? Alias aperiam
-                    blanditiis est, ex hic iste minus molestiae nobis nostrum odit perferendis porro quaerat rem
-                    repellat
-                    sapiente sequi sit. Accusamus animi assumenda in maiores recusandae! A autem commodi cumque ipsam
-                    iste
-                    magnam magni nostrum, numquam perferendis quibusdam rem sequi. Amet asperiores beatae consequatur
-                    cum
-                    dicta earum eligendi enim eum explicabo in libero, mollitia neque nihil nisi nulla odio, quaerat
-                    quas
-                    quo rem repellat sequi sint tempora temporibus totam unde? Maiores, nam?</p></div>
-            <div class="buttonrow">
-                <button class="btn" @click="showIntro=false">Close</button>
-            </div>
-        </div>
-        <div v-if="showYtOptIn" class="ytoptin popup">
-            <div>
-                <p>This play button allows you to watch the exact timestamp of this quote on YouTube.
-                    This means that the YouTube video is loaded on this page and data is sent to YouTube/Google.
-                    (<a href="https://lw1.at/i">Privacy Policy</a>)
-                </p>
-                <p>
-                    If you expected this to happen, simply continue and you won't be asked again. Otherwise you can
-                    abort or watch this timestamp directly on YouTube.
-                </p>
-            </div>
-            <div class="buttonrow">
-                <button class="btn" @click="showYtOptIn=false">abort</button>
-                <a class="btn" :href="ytLink" target="youtube" rel="noopener" @click="showYtOptIn=false">YouTube</a>
-                <button class="btn" @click="doYtOptIn">continue</button>
-            </div>
-        </div>
-        <div v-if="showYT" class="ytwrapper">
-            <button class="btn" @click="closeVideo">Hide</button>
-            <youtube :nocookie="true" ref="youtube" @ready="playVideo(false)" :width="ytWidth"></youtube>
-        </div>
-        <div class="inputlist">
-            <span>Search for</span>
-            <autocomplete :defaultValue="keyword" :search="suggest" @submit="handleSubmit"
-                          ref="searchInput"></autocomplete>
-            <span>up to episode </span>
-            <input title="search until episode number"
-                   class="form-control" type="number" v-model="episode"
-                   min="1" max="300">
-            <span>in</span>
-            <select title="campaign selection" class="custom-select" v-model="series">
-                <option v-for="series in serverData.series" v-bind:value="series.id">
-                    {{ series.title }}
-                </option>
-            </select>
-        </div>
-        <b-alert v-if="error" show :variant="error.status">{{error.message}}</b-alert>
-        <div class="entry" v-for="result in searchResult">
+    <div id="contentwrapper">
+        <div class="home">
             <div class="title">
-                <div>{{formatTimestamp(firstLine(result).starttime)}} {{episodeName(firstLine(result))}}</div>
-                <div class="buttons">
-                    <button class="btn" @click="playVideo(result)" title="View video on YouTube">
-                        <b-icon-play-fill></b-icon-play-fill>
-                    </button>
-                    <button class="btn" v-if="result.offset<10" @click="expand(result)" title="Load more context">
-                        +
-                    </button>
+                <h1>Critical Role Search</h1>
+                <span>Find your favourite Critical Role quote!</span>
+            </div>
+            <transition name="fade">
+                <div id="page-mask" v-if="showIntro || showYtOptIn"></div>
+            </transition>
+            <div v-if="showIntro" class="showIntro popup">
+                <div class="title"><h1>Critical Role Search</h1></div>
+                <div>
+                    <p>
+                        This website uses subtitles to allow a full text search through all
+                        <a href="https://critrole.com/">Critical Role</a> episodes.
+                    </p>
+                    <p>Subtitles for Campaign 1 and Campaign 2 up to Episode 54 by the
+                        <a href="https://crtranscript.tumblr.com/" target="_blank" rel="noopener">CR Transcript
+                            Team</a>,
+                        all other subtitles are by the Critical Role team.
+                    </p>
+                    <p>
+                        This website is licensed under the
+                        <a href="https://www.gnu.org/licenses/gpl-3.0.en.html">GPL-3.0</a>.
+                        You can find the source <a href="#">here</a>.
+                    </p>
+                    <dl>
+                        <dt>Fonts</dt>
+                        <dd>
+                            <a href="https://github.com/jonathonf/solbera-dnd-fonts" target="_blank" rel="noopener">
+                                by Solbera and Ryrok
+                            </a> (CC BY-SA 4.0)
+                        </dd>
+                        <dt>Design</dt>
+                        <dd>
+                            Inspired by
+                            <a href="https://homebrewery.naturalcrit.com/" target="_blank" rel="noopener">
+                                The Homebrewery
+                            </a>
+                            (MIT License)
+                        </dd>
+                    </dl>
+                </div>
+                <div class="buttonrow">
+                    <button class="btn" @click="showIntro=false">Close</button>
                 </div>
             </div>
-            <p :class="{line:true,note:line.isnote,meta:line.ismeta}" :style="{borderLeftColor:getColor(line)}"
-               v-for="line in result.lines" :key="line.id">
-                <span v-if="line.person" class="person">{{line.person.name}}: </span><span v-html="line.text"></span>
-            </p>
-        </div>
-        <details>
-            <summary>Raw Data</summary>
-            <pre>{{searchResult}}</pre>
-        </details>
+            <div v-if="showYtOptIn" class="ytoptin popup">
+                <div>
+                    <p>This play button allows you to watch the exact timestamp of this quote on YouTube.
+                        This means that the YouTube video is loaded on this page and data is sent to YouTube/Google.
+                        (<a href="https://lw1.at/i">Privacy Policy</a>)
+                    </p>
+                    <p>
+                        If you expected this to happen, simply continue and you won't be asked again. Otherwise you can
+                        abort or watch this timestamp directly on YouTube.
+                    </p>
+                </div>
+                <div class="buttonrow">
+                    <button class="btn" @click="showYtOptIn=false">abort</button>
+                    <a class="btn" :href="ytLink" target="youtube" rel="noopener" @click="showYtOptIn=false">YouTube</a>
+                    <button class="btn" @click="doYtOptIn">continue</button>
+                </div>
+            </div>
+            <div v-if="showYT" class="ytwrapper">
+                <button class="btn" @click="closeVideo">Hide</button>
+                <youtube :nocookie="true" ref="youtube" @ready="playVideo(false)" :width="ytWidth"></youtube>
+            </div>
+            <div class="inputlist">
+                <span>Search for</span>
+                <autocomplete :defaultValue="keyword" :search="suggest" @submit="handleSubmit"
+                              ref="searchInput"></autocomplete>
+                <span>up to episode </span>
+                <input title="search until episode number"
+                       class="form-control" type="number" v-model="episode"
+                       min="1" max="300">
+                <span>in</span>
+                <select title="campaign selection" class="custom-select" v-model="series">
+                    <option v-for="series in serverData.series" v-bind:value="series.id">
+                        {{ series.title }}
+                    </option>
+                </select>
+            </div>
+            <b-alert v-if="error" show :variant="error.status">{{error.message}}</b-alert>
+            <div class="entry" v-for="result in searchResult">
+                <div class="title">
+                    <div>{{formatTimestamp(firstLine(result).starttime)}} {{episodeName(firstLine(result))}}</div>
+                    <div class="buttons">
+                        <button class="btn" @click="playVideo(result)" title="View video on YouTube">
+                            <b-icon-play-fill></b-icon-play-fill>
+                        </button>
+                        <button class="btn" v-if="result.offset<10" @click="expand(result)" title="Load more context">
+                            +
+                        </button>
+                    </div>
+                </div>
+                <p :class="{line:true,note:line.isnote,meta:line.ismeta}" :style="{borderLeftColor:getColor(line)}"
+                   v-for="line in result.lines" :key="line.id">
+                    <span v-if="line.person" class="person">{{line.person.name}}: </span><span
+                        v-html="line.text"></span>
+                </p>
+            </div>
+            <details>
+                <summary>Raw Data</summary>
+                <pre>{{searchResult}}</pre>
+            </details>
 
+        </div>
+        <footer>
+            <button @click="showIntro=true" class="btn btn-link">About this website</button>
+            <a href="https://lw1.at">My other Projects</a>
+            <a href="https://lw1.at/i">Privacy Policy</a>
+        </footer>
     </div>
 </template>
 
@@ -135,7 +140,7 @@
     },
     data() {
       return {
-        serverData : require("../../data.json") as ServerData,
+        serverData: require("../../data.json") as ServerData,
         searchResult: [] as Result[],
         keyword: this.$route.params.keyword,
         series: this.$route.params.series,
