84f5ff21657e25d9c1696f5120fafc596d2bec45
Lukas Winkler <git@lw1.at>
2020-08-07 18:50:41 +0200

    add gunicorn config

---
+++ gunicorn.py
@@ -0,0 +1,7 @@
+# import multiprocessing
+
+pidfile="/srv/server/crsearch/runtime/pid"
+bind="unix:/srv/server/crsearch/runtime/socket"
+proc_name="crsearch"
+worker_tmp_dir="/dev/shm"
+# workers = multiprocessing.cpu_count() * 2 + 1
